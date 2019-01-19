7c12789ab86c7d37bff3f6b8f489a629cefba98b
parth7676 <parth7676@gmail.com>
2019-01-19 14:46:42 -0500

    Initial Commit

---
+++ flaskblog/main/routes.py
@@ -0,0 +1,15 @@
+from flask import render_template, request, Blueprint
+
+main: Blueprint = Blueprint('main', __name__)
+
+
+@main.route("/")
+@main.route("/home")
+def home():
+    page = request.args.get('page', 1, type=int)
+    return render_template('home.html')
+
+
+@main.route("/about")
+def about():
+    return render_template('about.html')
---
+++ run.bat
@@ -0,0 +1,3 @@
+SET FLASK_APP=run.py
+SET FLASK_DEBUG=True
+C:\Users\parth\PycharmProjects\uFlaskSample\venv\Scripts\python.exe -m flask run
